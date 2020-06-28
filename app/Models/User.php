1dccfafb7e0197faa4f98274dd4fb8b384ad20e5
vagrant <vagrant@homestead>
2020-06-28 04:46:15 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,7 +68,10 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
     }
 
     //用户粉丝
@@ -92,7 +95,7 @@ class User extends Authenticatable
     {
         $user_ids = is_array($user_ids) ? $user_ids : [$user_ids];
 
-        $this->followings()->sync($user_ids,false);
+        $this->followings()->sync($user_ids, false);
     }
 
     public function unfollow($user_ids)
